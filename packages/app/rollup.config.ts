1e14959fda9e49620c10355500d8ef5942fe222c
Xavier HEN <uminily@gmail.com>
2021-07-10 11:46:30 +0200

    build(app): rollup config with this first one

---
+++ packages/app/rollup.config.ts
@@ -0,0 +1,11 @@
+import typescript from 'rollup-plugin-typescript2'
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs',
+    sourcemap: true
+  },
+  plugins: [typescript()]
+}
