fca96817398369eddab1c2ba1d07cada0aa61497
Xavier HEN <uminily@gmail.com>
2021-07-10 18:28:57 +0200

    chore(crud): build for Crud missing type

--- packages/crud/src/index.ts
+++ packages/crud/src/index.ts
@@ -1,3 +1,5 @@
 export * from './decorators'
 export * from './interfaces'
 export * from './validators'
+
+export * from './CrudService'
--- packages/crud/src/interfaces/index.ts
+++ packages/crud/src/interfaces/index.ts
@@ -1,1 +1,2 @@
 export * from './Association'
+export * from './CrudControllerInterface'
