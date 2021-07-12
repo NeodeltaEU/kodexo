4f593b2823027d3e648c8d25467f84a54f003a6d
Xavier HEN <uminily@gmail.com>
2021-07-12 17:55:32 +0200

    build: push so many things again

--- packages/crud/tests/mocks/Server.ts
+++ packages/crud/tests/mocks/Server.ts
@@ -6,22 +6,31 @@ import { Car } from './features/cars/entities/car.entity'
 import { Dealership } from './features/dealerships/entities/dealership.entity'
 import { User } from './features/users/entities/user.entity'
 import { Workshop } from './features/workshops/entities/workshop.entity'
-import { Inject } from '../../../injection/dist'
+
+const { POSTGRES_HOST, POSTGRES_PORT } = process.env
 
 @Configuration({
   port: 4000,
   mikroORM: {
+    host: POSTGRES_HOST || 'localhost',
     dbName: 'kodex',
     type: 'postgresql',
     user: 'kodex',
     password: 'kodex',
-    port: 5433,
+    port: parseInt(POSTGRES_PORT as string, 10) || 5433,
     entities: [Car, Workshop, Dealership, User],
     debug: false
   },
   middlewares: [RequestContextMiddleware()],
-  debugClient: false,
-  debugServer: true,
-  skipClientError: true
+
+  debug: {
+    skipClientRequestError: true,
+    displayErrorsOnServerCli: false,
+    displayErrorsOnClientResponse: false
+  },
+
+  logs: {
+    request: false
+  }
 })
 export class Server implements ServerHooks {}
