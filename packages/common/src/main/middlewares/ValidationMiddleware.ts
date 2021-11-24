8152b25282f08a089475a06395fa6960373527f9
Xavier HEN <uminily@gmail.com>
2021-11-24 14:54:46 +0100

    fix(common): errors handling for validations

--- packages/common/src/main/middlewares/ValidationMiddleware.ts
+++ packages/common/src/main/middlewares/ValidationMiddleware.ts
@@ -6,6 +6,36 @@ import { validateOrReject, ValidationError } from 'class-validator'
 import { plainToInstance } from 'class-transformer'
 import { Class } from 'type-fest'
 
+function handleChildrenErrors(
+  result: Array<any>,
+  errors: ValidationError[],
+  propertiesTree: string
+) {
+  if (!errors.length) return result
+
+  errors.forEach(error => {
+    const { property, constraints, children } = error
+
+    if (children?.length) {
+      handleChildrenErrors(result, children, `${propertiesTree}.${property}`)
+    }
+
+    if (!constraints) {
+      result.push({
+        property: `${propertiesTree}.${property}`,
+        message: 'An error has occured during validation.'
+      })
+      return
+    }
+
+    Object.values(constraints).forEach(message => {
+      result.push({ property: `${propertiesTree}.${property}`, message })
+    })
+  })
+
+  return result
+}
+
 export class ValidationMiddleware implements MiddlewareHandling {
   constructor(private dtoToken: Class) {}
 
@@ -30,8 +60,8 @@ export class ValidationMiddleware implements MiddlewareHandling {
 
         // TODO: Handle nested errors with children & make some tests
         if (!constraints) {
-          result.push({ property, message: 'An arror has occured during validation' })
-          return result
+          result.push({ property, message: 'An error has occured during validation.' })
+          return handleChildrenErrors(result, validationError.children, property)
         }
 
         Object.values(constraints).forEach(message => {
@@ -42,7 +72,7 @@ export class ValidationMiddleware implements MiddlewareHandling {
       }, [])
 
       throw HttpError.UnprocessableEntity({
-        message: `An error occurred with the body passed, see below.`,
+        message: `An error has occurred with the body passed, see below.`,
         details: errors
       })
     }
