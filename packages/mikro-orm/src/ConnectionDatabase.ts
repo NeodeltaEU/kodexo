98e53381451499dbaf56910c65965292c98e7a58
Xavier HEN <uminily@gmail.com>
2022-08-19 17:09:34 +0200

    fix(mikro-orm): apply migrations config by default

--- packages/mikro-orm/src/ConnectionDatabase.ts
+++ packages/mikro-orm/src/ConnectionDatabase.ts
@@ -115,6 +115,7 @@ export class ConnectionDatabase {
     })
 
     this.settings.migrations = {
+      ...(this.settings.migrations || {}),
       migrationsList: [...new Set(rawMigrations)]
     }
 
