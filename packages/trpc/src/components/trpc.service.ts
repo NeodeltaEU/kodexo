678125dbeb1f993f84ddbb56691d643cd595a276
Xavier HEN <uminily@gmail.com>
2022-10-03 14:47:09 +0200

    feat(trpc): add superjson

--- packages/trpc/src/components/trpc.service.ts
+++ packages/trpc/src/components/trpc.service.ts
@@ -3,11 +3,14 @@ import { Service } from '@kodexo/common'
 import { ConfigurationService } from '@kodexo/config'
 import { Inject, OnProviderInit, Registry } from '@kodexo/injection'
 import { initTRPC } from '@trpc/server'
+import superjson from 'superjson'
 import { createTinyHttpMiddleware } from '../main/adapters/tinyhttp'
 
 @Service()
 export class TRPCService implements OnProviderInit {
-  public static t = initTRPC.create()
+  public static t = initTRPC.create({
+    transformer: superjson
+  })
 
   constructor(
     @Inject private readonly $routes: RoutesService,
