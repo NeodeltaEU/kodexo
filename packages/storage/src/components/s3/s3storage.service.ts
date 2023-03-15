a36415b935d7414f6fd865bf059b660434e09120
Xavier HEN <uminily@gmail.com>
2023-03-15 15:48:17 +0100

    refactor(storage): expose client from S3 storage

--- packages/storage/src/components/s3/s3storage.service.ts
+++ packages/storage/src/components/s3/s3storage.service.ts
@@ -14,7 +14,7 @@ import { StorageService } from '../../main'
 
 @Service()
 export class S3StorageService extends StorageService {
-  private client: S3Client
+  public readonly client: S3Client
   private cdnRootUrl?: string
   private bucket: string
 
